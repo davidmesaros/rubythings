29abf68d187d3f90a28b7624f2b49a4a7985f154
davidmesaros <david.mesaros@hotmail.com>
2014-09-25 23:30:55 +1000

    rubythehardway

---
+++ rubythehardway.rb
@@ -0,0 +1,182 @@
+# p "Hello world"
+# #p 'I "said" dont touch'
+# #+plus
+# #<= less-then-equal
+# #>=greater-then-equal
+
+# p "I will now count my chickens:"
+
+# p "Hens#{25+ 30 / 6}"
+# p "Roosters #{100 - 25 * 3 % 4.to_f}"
+# p " Now I will count the eggs:"
+
+# p 3 + 2 < 5 - 7
+
+# p "Is it greater? #{5 > -2}"
+
+# p "what is a module #{ 25 % 3 }"
+
+# p"================================="
+
+# cars = 100
+# space_in_a_car = 4.0
+# drivers = 30
+
+# cars_driven = drivers
+
+# carpool_capacity = cars_driven * space_in_a_car
+
+# pounds = 1
+# kg 	= 0.45
+
+# pounds_to_kg = 2.22
+# p "please enter your weight in pounds"
+# pound = gets.chomp.to_i
+# kg = pound/pounds_to_kg
+
+# p "I weigh #{ pound } pounds that means I weigh in #{kg.to_i}kg's "
+
+# puts "There are #{cars} cars available"
+
+# p "We can transport #{ carpool_capacity.to_i } people today"
+
+# types_of_people = 10
+# x = "there are #{types_of_people}"
+# binary = "binary"
+# do_not = "don't"
+
+# y = "those who know #{binary} and those who #{do_not}"
+
+# p x
+# p y
+
+
+# puts "Its Fleece was white as #{'snow'}"
+
+# end1 = "C"
+# end2 = "h"
+# end3 = "e"
+# end4 = "e"
+# end5 = "s"
+# end6 = "e"
+# end7 = "B"
+# end8 = "u"
+# end9 = "r"
+# end10 = "g"
+# end11 = "e"
+# end12 = "r"
+
+# print end1 + end2 + end3 + end4 + end5 + end6
+# puts end7 + end8 + end9 + end10 + end11 + end12
+
+# formatter = "%{first} %{second} %{third} %{fourth}"
+
+# puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+# puts formatter % {first: "one", second: 'two', third: 'three', fourth: 'fourth'}
+# puts formatter % {first: true, second: false, third: true, fourth: false }
+# puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter }
+# puts formatter % {
+# 	first: "I had this thing.",
+# 	second: "That you could type up right",
+# 	third: "But it didn't sing ",
+# 	fourth: "So I said goodnight."
+# }
+
+# days = " Mon, Tue, Wed, Thurs, Fri, Sat, Sun"
+# months = "\nJan\nFeb\nMar\nApri\nMay"
+
+# puts " Here are the days #{days}"
+# puts " Here are the months #{months}"
+
+# puts " " "
+# There's something going on here.
+# With the three double-quotes
+# " " "
+
+# print "How old are you"
+# age = gets.chomp
+# print "how tall are you"
+# height = gets.chomp
+
+# puts "So you are #{age} years old and your hieght is #{height}"
+
+# print "Give me a number"
+# number = gets.chomp.to_i
+
+# bigger = number * 100
+# puts "A bigger numebr is #{bigger}"
+
+
+# p "give me float"
+# float = gets.chomp
+# number = float.to_f/10
+
+# p number.to_f.round(2)
+
+
+
+# first, second, third = ARGV
+
+# puts "Your first variable is: #{first}"
+# puts "Your second varible is: #{second}"
+# puts "Your third varible is: #{third}"
+
+# p "what is favourite this"
+# p fourth = gets.chomp
+
+# p "my favourite color is #{fourth}"
+
+# user_name = ARGV.last
+# david = '> '
+
+# puts "Hi #{user_name}"
+# puts "I'd like to ask you few questions."
+# puts "Do you like me #{user_name}?", david
+# likes = $stdin.gets.chomp
+
+# puts "Where do you live #{user_name} ", david
+
+# lives = $stdin.gets.chomp
+
+# puts "what kind of computer do you have", david
+# computer =$stdin.gets.chomp
+
+# puts " " "
+# Alright, so you said #{likes} about me.
+# You Live in #{lives}. Not sure where that is.
+# And you have a #{computer} computer. Nice.
+# " " "
+
+#declaring varible on the first argument.
+# filename = ARGV.first
+
+# txt = open(filename)
+
+# puts "Here's your file #{filename}"
+# print txt.read
+
+# 	print "Type the filename again: "
+# 	file_again = $stdin.gets.chomp
+
+# 	txt_again = open(file_again)
+
+# 	print txt_again.read
+
+# 	array = ['glob pish' => 9,
+# 	'prok' => 5,
+# 	'glob prok' => 4,
+#  	'glob' => 1]
+
+#   x	= array.gsub(/(\w+)\*?(\w+)/)
+
+# p x
+
+
+
+
+
+
+
+
+
+
